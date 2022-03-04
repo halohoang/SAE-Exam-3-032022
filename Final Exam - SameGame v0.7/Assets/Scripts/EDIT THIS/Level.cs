77f17806727c5b67d5462d271f88b5e9cc8c157f
Ahmed Tlili <Ahmedtlilivipor@gmail.com>
2022-03-04 19:09:19 +0100

    Update Level.cs

--- Final Exam - SameGame v0.7/Assets/Scripts/EDIT THIS/Level.cs
+++ Final Exam - SameGame v0.7/Assets/Scripts/EDIT THIS/Level.cs
@@ -65,8 +65,12 @@ public class Level
 
         // ***** Students Start here ******
         element = new Element(element.Visuals, element.ElementType);
-        
-        
+
+        //for (int x = 0; x < 16; x++)
+        //{
+        //    for (int y = 0; y < 16; y++)
+        //    grid.SetElement(,element);
+        //}
 
     }
 
